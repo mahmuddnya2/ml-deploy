c64e4e471db3f1a37a5da5a7e532f47b52ec9132
mahmudawaludin17 <mahmudawaludin17@gmail.com>
2024-05-21 19:25:50 +0700

    fix app

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 app.config['ALLOWED_EXTENSIONS'] = set(["png", "jpg", "jpeg"])
 app.config['UPLOAD_FOLDER'] = 'static/uploads/'
 app.config['MODEL_CLASSIFICATION'] = './models/model.h5'
-app.config['GCS_CREDENTIALS'] = './credentials/gcs.json'
+app.config['GCS_CREDENTIALS'] = './credentials/gcs2.json'
 
 model_classification = tf.keras.models.load_model(
     app.config['MODEL_CLASSIFICATION'], compile=False)
@@ -96,4 +96,4 @@ def predictions():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5001)))
