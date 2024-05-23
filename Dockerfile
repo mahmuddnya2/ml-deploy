000ecae3ad3fa556a90f2c9d484b976a0d723b5d
mahmudawaludin17 <mahmudawaludin17@gmail.com>
2024-05-23 19:52:49 +0700

    port dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,11 @@
 FROM python:3.10-slim
-
 ENV PYTHONBUFFERED True
 ENV APP_HOME /app
 WORKDIR $APP_HOME
 COPY . ./
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
+ENV PORT 8080
+
 EXPOSE 8080
-CMD ["python", "app.py"]
+CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,28 +1,29 @@
+from werkzeug.utils import secure_filename
+from dotenv import load_dotenv
+from google.cloud import storage
+from flask import Flask, jsonify
+from flask import request
+from PIL import Image
+from http import HTTPStatus
+import random
+import tensorflow as tf
 import numpy as np
 import os
-import tensorflow as tf
-import random
-from http import HTTPStatus
-from PIL import Image
-from flask import Flask, jsonify, request
-from google.cloud import storage
-from dotenv import load_dotenv
-from werkzeug.utils import secure_filename
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 load_dotenv()
 
 app = Flask(__name__)
 
-app.config['ALLOWED_EXTENSIONS'] = set(["png", "jpg", "jpeg"])
+app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg'])
 app.config['UPLOAD_FOLDER'] = 'static/uploads/'
 app.config['MODEL_CLASSIFICATION'] = './models/model.h5'
-app.config['GCS_CREDENTIALS'] = './credentials/gcs2.json'
+app.config['GCS_CREDENTIALS'] = './credentials/gcs.json'
 
 model_classification = tf.keras.models.load_model(
     app.config['MODEL_CLASSIFICATION'], compile=False)
 
-bucket_name = os.environ.get('BUCKET_NAME', 'ml-deploy-110803')
+bucket_name = os.environ.get('BUCKET_NAME', 'bjir-sharing-session')
 client = storage.Client.from_service_account_json(
     json_credentials_path=app.config['GCS_CREDENTIALS'])
 bucket = storage.Bucket(client, bucket_name)
@@ -30,48 +31,38 @@ bucket = storage.Bucket(client, bucket_name)
 
 def allowed_file(filename):
     return '.' in filename and \
-        filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
 
-classes = ['Coris julis', 'Trigloporus lastoviza', 'Mugil cephalus',]
+classess = ["Coris julis", "Trigloporus lastoviza", "Mugil cephalus",]
 
 
 @app.route('/', methods=['GET'])
 def index():
     return jsonify({
-        'message': "hello world"
+        'Message': 'Hello World!',
     }), HTTPStatus.OK
 
 
-@app.route('/predict', methods=['POST'])
-def predictions():
+@app.route('/prediksi', methods=['POST'])
+def predict():
     if request.method == 'POST':
-        req_image = request.files['image']
-        if req_image and allowed_file(req_image.filename):
-            filename = secure_filename(req_image.filename)
-            req_image.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        reqImage = request.files['image']
+        if reqImage and allowed_file(reqImage.filename):
+            filename = secure_filename(reqImage.filename)
+            reqImage.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-
             img = Image.open(image_path).convert('RGB')
             img = img.resize((160, 160))
-
             x = tf.keras.preprocessing.image.img_to_array(img)
             x = np.expand_dims(x, axis=0)
-            x = x / 225
-
-            classification_result = model_classification.predict(
+            x = x / 255
+            classificationResult = model_classification.predict(
                 x, batch_size=1)
             result = {
-                'class': classes[np.argmax(classification_result)],
-                'probability': str(np.max(classification_result))
+                'class': classess[np.argmax(classificationResult)],
+                'probability': str(np.max(classificationResult))
             }
-            # array - 1 digunakan untuk mengambil nilai array paling terakhir
-            image_name = image_path.split('/')[-1]
-            blob = bucket.blob(
-                'images/' + str(random.randint(10000, 999999))+image_name)
-            blob.upload_from_filename(image_path)
-            os.remove(image_path)
-
             return jsonify({
                 'status': {
                     'code': HTTPStatus.OK,
@@ -83,7 +74,7 @@ def predictions():
             return jsonify({
                 'status': {
                     'code': HTTPStatus.BAD_REQUEST,
-                    'message': 'File extension not allowed'
+                    'message': 'Invalid file format. Please upload a JPG, JPEG, or PNG image.'
                 }
             }), HTTPStatus.BAD_REQUEST
     else:
